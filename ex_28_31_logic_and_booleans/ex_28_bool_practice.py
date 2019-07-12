12341cb8b676b23254c6da0c5f85d41c609d2594
oshkola <olsh91@gmail.com>
2019-07-12 10:34:33 +0200

    add ex_28_31_booleans

---
+++ ex_28_31_logic_and_booleans/ex_28_bool_practice.py
@@ -0,0 +1,22 @@
+print True and True        #True
+print True and False       #False
+
+print 1 == 1 and 2 == 1    #False
+print "test" == "test"     #True
+
+print 1 == 1 or 2 != 1     #True
+print True and 1 == 1      #True
+
+print False and 0 != 0     #False
+
+print True or 1 == 1       #True
+
+print "test" == "testing"  #False 
+print "test" == 1          #False
+
+print not (True and False) #True
+print not (1==1 or 0 != 1) #False
+
+print 3 == 3 or ("test" == "testing") #True
+
+print 6 <= 24 / 4          #True
---
+++ ex_28_31_logic_and_booleans/ex_29_if_statement.py
@@ -0,0 +1,27 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+	print "Too many cats"
+
+if people > cats:
+	print "Not many cats"
+
+if people < dogs:
+	print "Too many dogs"
+
+if people > dogs:
+	print "Not many dogs"
+
+dogs += 5
+
+
+if people <= dogs:
+	print "People <= dogs"
+
+if people >= dogs:
+	print "People >= dogs"
+
+if people == dogs:
+	print "People are dogs :)"
